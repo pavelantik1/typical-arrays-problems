124f94a873b50cc9c59e542fdbc6ae6a63a1f58a
Pavlo Sliusarchuk <sksuzh@gmail.com>
2020-10-02 18:21:27 +0300

    feat: add min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array=[]) {
+ if(!array.length) return 0; 
+ return array.sort((x,y) => x-y)[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array=[]) {
+  if(!array.length) return 0;  
+  return array.sort((x,y) => y-x)[0];
+  
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array=[]) {
+  if(!(array.length)) return 0;  
+  return array.reduce( (sum, item) => sum +=item, 0)/array.length;
+   
 }
